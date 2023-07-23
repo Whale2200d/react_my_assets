23222db32ecaab2e6f8a2a2a20d49c891699af3d
Whale2200d <whale2200d@gmail.com>
2023-07-23 16:28:59 +0900

    Refactor : useHooks 목록 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import ContextPage from './page/ContextPage';
 import DataPost from './components/DataCRUD/DataPost/DataPost';
 // import Chat from './components/Chat/Chat';
 import ButtonRendering from './components/ButtonRendering';
-import UserParameters from './components/UseParameters';
+// import UserParameters from './components/UseParameters';
 import DataChildToParent from './components/DataChildToParent/Parent';
+import UseState1 from './useHooks/UseState/UseState1';
+import UseState2 from './useHooks/UseState/UseState2';
+import UseEffect1 from './useHooks/UseEffect/UseEffect1';
+import UseEffect2 from './useHooks/UseEffect/UseEffect2';
+import UseRef1 from './useHooks/UseRef/UseRef1';
+import UseRef2 from './useHooks/UseRef/UseRef2';
+import UseRef3 from './useHooks/UseRef/UseRef3';
 
 function App() {
   return (
@@ -15,8 +22,15 @@ function App() {
         <Route path="/post" element={<DataPost />} />
         {/* <Route path="/chat" element={<Chat />} /> */}
         <Route path="/buttonrendering" element={<ButtonRendering />} />
-        <Route path="/useparameter" element={<UserParameters />} />
+        {/* <Route path="/useparameter" element={<UserParameters />} /> */}
         <Route path="/datachildtoparent" element={<DataChildToParent />} />
+        <Route path="/usestate1" element={<UseState1 />} />
+        <Route path="/usestate2" element={<UseState2 />} />
+        <Route path="/useeffect1" element={<UseEffect1 />} />
+        <Route path="/useeffect2" element={<UseEffect2 />} />
+        <Route path="/useref1" element={<UseRef1 />} />
+        <Route path="/useref2" element={<UseRef2 />} />
+        <Route path="/useref3" element={<UseRef3 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/page/ContextPage/index.tsx
+++ src/page/ContextPage/index.tsx
@@ -11,6 +11,15 @@ function ContextPage() {
         <Link to="buttonrendering">3. ButtonRendering</Link>
         <Link to="useparameter">4. UseParameters</Link>
         <Link to="datachildtoparent">5. DataChildToParent</Link>
+        <br />
+        <h1>useHooks</h1>
+        <Link to="usestate1">1-1. useState1</Link>
+        <Link to="usestate2">1-2. useState2</Link>
+        <Link to="useeffect1">2-1. useEffect1</Link>
+        <Link to="useeffect2">2-2. useEffect2</Link>
+        <Link to="useref1">3-1. useRef1</Link>
+        <Link to="useref2">3-2. useRef2</Link>
+        <Link to="useref3">3-3. useRef3</Link>
       </S.Container>
     </>
   );
